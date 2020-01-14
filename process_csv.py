4766779aa2ff56c80a382a804af7a28096eef21d
Michal Mildner <michal.mildner@flightdataservices.com>
2020-01-14 22:24:42 +0000

    add requirements 3 and 4 add unit tests

--- process_csv.py
+++ process_csv.py
@@ -1,12 +1,16 @@
 import csv
 import os
+
+from datetime import datetime
+from pprint import pprint
 from tabulate import tabulate
 
 DIR = './data'
 DATASET = 'dataset.csv'
+START_DATE = datetime(1999, 6, 1, 0, 0)
+END_DATE = datetime(2007, 8, 31, 0, 0)
 
 def least_five_rent(tenant_list, header):
-    # Requirement 1
     # sort the list in place, no need to reverse as the requirements specify ascending order
     tenant_list.sort(key=lambda x: x[-1:])
 
@@ -14,16 +18,24 @@ def least_five_rent(tenant_list, header):
 
 
 def lease_25_years(tenant_list, header):
-    # Requirement 2
-    # list comprehension
     mast_25y = [t for t in tenant_list if t[9] == 25]
     total_rent = sum(t[10] for t in mast_25y)
 
     return mast_25y, total_rent
 
 
-# def mast_count(tenant_list, header):
-#
+def mast_count(tenant_list):
+    transposed_list = list(map(list, zip(*tenant_list)))[6] # 7th column is tenant name
+    tenants_list = list(dict.fromkeys(transposed_list))
+    mast_count = {t: transposed_list.count(t) for t in tenants_list}
+
+    return mast_count
+
+
+def date_range(tenant_list, start_date, end_date):
+    return [t for t in tenant_list if (t[7]>start_date and t[7]<end_date)]
+
+
 def prepare_data(dir, dataset):
     # Read csv, save header to separate variable
     os.chdir(dir)
@@ -38,10 +50,13 @@ def prepare_data(dir, dataset):
             else:
                 tenant_list.append(row)
 
-    # convert values to ints and floats and dates to datetime objects
+    # convert values used to ints and floats
     for t in tenant_list:
+        t[7] = datetime.strptime(t[7], '%d %b %Y')
+        t[8] = datetime.strptime(t[8], '%d %b %Y')
+        t[9] = int(t[9])  # lease duration
         t[10] = float(t[10]) #rent
-        t[9] = int(t[9]) #lease duration
+
 
     return header, tenant_list
 
@@ -62,6 +77,15 @@ def main():
     print(tabulate(lease_25y, headers=header))
     print('Total rent the above tenants pay: £{0:.2f}'.format(total_25y))
 
+    # Requirement 3
+    tenant_mast_count = mast_count(tenant_list)
+    print('\nMast count per tenant: \n')
+    print(tabulate(tenant_mast_count.items(), headers=['Tenant Name', 'Mast Count']))
+
+    # Requirement 4
+    dates_limited = date_range(tenant_list, START_DATE, END_DATE)
+    print('\nTenants with lease start date between 1st of June 1999 and 31st of August 2007: \n')
+    print(tabulate(dates_limited, headers=header))
 
 if __name__ == '__main__':
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,27 @@
 import unittest
 
-from process_csv import prepare_data, least_five_rent, lease_25_years
+from datetime import datetime
+
+from process_csv import prepare_data, least_five_rent, lease_25_years, mast_count, date_range
 
 test_data = [['Beecroft Hill', 'Broad Lane', '', '', 'LS13', 'Beecroft Hill - Telecom App',
-                   'Arqiva Services ltd', '01 Mar 1994', '28 Feb 2058', 64, 23950.00],
+                   'Arqiva Services ltd', datetime(1994, 3, 1, 0, 0), datetime(2058, 2, 28, 0, 0), 64, 23950.00],
                   ['Potternewton Crescent', 'Potternewton Est Playing Field', '', '', 'LS7',
-                   'Potternewton Est Playing Field', 'Arqiva Ltd', '24 Jun 1999', '23 Jun 2019', 20, 6600.00],
+                   'Potternewton Est Playing Field', 'Arqiva Ltd', datetime(1999, 6, 24, 0, 0),
+                   datetime(2019, 6, 23, 0, 0), 20, 6600.00],
                   ['Seacroft Gate (Chase) - Block 2', 'Telecomms Apparatus', 'Leeds', '', 'LS14',
-                   'Seacroft Gate (Chase) block 2-Telecom App.', 'Vodafone Ltd.', '30 Jan 2004',
-                   '29 Jan 2029', 25, 12250.00],
+                   'Seacroft Gate (Chase) block 2-Telecom App.', 'Vodafone Ltd.', datetime(2004, 1, 30, 0, 0),
+                   datetime(2029, 1, 29, 0, 0), 25, 12250.00],
                   ['Queenswood Heights', 'Queenswood Heights', 'Queenswood Gardens', 'Headingley', 'Leeds',
-                   'Queenswood Hgt-Telecom App.', 'Vodafone Ltd', '08 Nov 2004', '07 Nov 2029', 25, 9500.00],
+                   'Queenswood Hgt-Telecom App.', 'Vodafone Ltd', datetime(2004, 11, 8, 0, 0),
+                   datetime(2029, 11, 7, 0, 0), 25, 9500.00],
                   ['Armley - Burnsall Grange', 'Armley', 'LS13', '', '', 'Burnsall Grange CSR 37865', 'O2 (UK) Ltd',
-                   '26 Jul 2007', '25 Jul 2032', 25, 12000.00]]
+                   datetime(2007, 7, 26, 0, 0), datetime(2032, 7, 25, 0, 0), 25, 12000.00]]
 test_header = ['Property Name', 'Property Address [1]', 'Property  Address [2]', 'Property Address [3]',
                     'Property Address [4]', 'Unit Name', 'Tenant Name', 'Lease Start Date', 'Lease End Date',
                     'Lease Years', 'Current Rent']
+start_date = datetime(1999, 6, 1, 0, 0)
+end_date = datetime(2007, 8, 31, 0, 0)
 
 class TestProcessCsv(unittest.TestCase):
 
@@ -26,7 +32,11 @@ class TestProcessCsv(unittest.TestCase):
         DATASET = 'dataset.csv'
         header, tenant_list = prepare_data(DIR, DATASET)
         self.assertEqual(header[0], 'Property Name')
-        self.assertEqual(tenant_list[10][0], 'Gledhow Towers - Telecom App')
+        self.assertEqual(tenant_list[0][7], datetime(1994, 3, 1, 0, 0))
+        self.assertEqual(tenant_list[0][8], datetime(2058, 2, 28, 0, 0))
+        self.assertEqual(tenant_list[0][10], 23950.0)
+        self.assertEqual(tenant_list[0][0], 'Beecroft Hill')
+        self.assertEqual(len(tenant_list), 42) # the answer to life, universe, and everything, and also the length of this list
 
 
     def test_five_rent(self):
@@ -41,5 +51,15 @@ class TestProcessCsv(unittest.TestCase):
         self.assertEqual(len(mast_25y), 3)
 
 
+    def test_mast_count(self):
+        count = mast_count(test_data)
+        self.assertEqual(count, {'Arqiva Services ltd': 1, 'Arqiva Ltd': 1, 'Vodafone Ltd.': 1,
+                                 'Vodafone Ltd': 1, 'O2 (UK) Ltd': 1})
+
+    def test_dates(self):
+        dates = date_range(test_data, start_date, end_date)
+        self.assertEqual(len(dates), 4)
+
+                         )
 if __name__ == '__main__':
     unittest.main()
